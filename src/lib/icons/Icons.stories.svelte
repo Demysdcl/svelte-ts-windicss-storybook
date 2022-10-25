9fd1161d51a3bc779ead750dd0c042263107951b
demyslima <demys.lima@contasimples.com>
2022-10-25 12:01:03 -0300

    chore: storybook typescript support

---
+++ src/lib/icons/Icons.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+
+	import Icons from './Icons.svelte'
+</script>
+
+<Meta title="Icons" component={Icons} />
+
+<Template let:args>
+	<Icons {...args} />
+</Template>
+
+<Story name="Rounded" args={{ name: 'mdiHome' }} />
