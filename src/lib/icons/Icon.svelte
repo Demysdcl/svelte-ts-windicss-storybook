43ea8e41f4866210fde3790cbe9c4b5f03cac1dc
demyslima <demys.lima@contasimples.com>
2022-10-25 10:56:50 -0300

    feat: icon component

---
+++ src/lib/icons/Icon.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import * as MaterialIcons from '@mdi/js'
+	import type { IconsType } from '../shared/types'
+
+	export let name: IconsType
+	export let size: number | string = 16
+	export let color: string = 'currentColor'
+
+	const width = typeof size === 'string' ? size : `${Number(size) / 16}rem`
+
+	const path = MaterialIcons[name]
+</script>
+
+<svg
+	viewBox="0 0 24 24"
+	{width}
+	height={width}
+	fill={color}
+	class="align-middle"
+>
+	<path d={path} />
+</svg>
